4f533c0fe1a135f81999ae923182406a726cd73a
Yu Hin Tsui <37496836+HinsonTsui@users.noreply.github.com>
2018-04-25 15:54:05 +1000

    add btn function

--- app/src/main/java/e/dell/test/MainActivity.java
+++ app/src/main/java/e/dell/test/MainActivity.java
@@ -1,13 +1,33 @@
 package e.dell.test;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Context mContext = MainActivity.this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    private void openMap(){
+        Button btn = (Button) findViewById(R.id.button1);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mContext, MapsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+
+
 }
